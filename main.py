d77a18e83e6c26b5cd5272cafe84963d40f05ff4
Bill Slater <william.slater527@gmail.com>
2019-04-27 11:08:58 -0500

    Added user signup validation

--- main.py
+++ main.py
@@ -61,6 +61,67 @@ def blog_page():
         blogs = Blog.query.all()
         return render_template('blog.html', blogs=blogs)
 
+@app.route('/login', methods=['POST', 'GET'])
+def log_in():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            return redirect('/newpost')
+        elif  user and user.password != password:
+            flash('User password incorrect.', 'error')
+            return redirect('/login')
+        else:
+            flash('That Username does not exist.', 'error')
+            return redirect('/login')
+    return render_template('login.html')
+
+
+def proper_length(entry):
+    
+    if len(entry) > 2 and len(entry) < 21:
+        return True
+
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        if not proper_length(username):
+            flash('Your username must be between 3-20 characters.', 'error')
+            return redirect('/signup')
+
+        if not proper_length(password):
+            flash('Your password must be between 3-20 characters.', 'error')
+            return redirect('/signup')
+
+        if username == '' or password == '' or verify == '':
+            flash('One of your fields is invalid.', 'error')
+            return redirect('/signup')
+
+        if password != verify:
+            flash('Your passwords do not match.', 'error')
+            return redirect('/signup')
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            #todo user better response messaging
+            return "<h1>Duplicate user</h1>"
+    return render_template('signup.html')
+
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
